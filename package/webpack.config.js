efc624c92cba3951d32f51065a2c6a2d37587fb5
Noah Mason <noah@dreamsicle.io>
2024-03-09 00:34:59 -0500

    Adds ThemePackageBuilderPlugin for webpack.

--- package/webpack.config.js
+++ package/webpack.config.js
@@ -1,23 +1,146 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const wpConfig = require('@wordpress/scripts/config/webpack.config');
-const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
-const path = require('path');
-const execSync = require('child_process').execSync;
+// @ts-check
+
+import fs from 'fs';
+import path from 'path';
+import webpack from 'webpack'; /* eslint-disable-line import/no-extraneous-dependencies */
+import wpConfig from '@wordpress/scripts/config/webpack.config.js';
+import RemoveEmptyScriptsPlugin from 'webpack-remove-empty-scripts';
+import DirArchiver from 'dir-archiver';
+import { execSync } from 'child_process';
 
 const themeDirectory = process.cwd();
+const pkgPath = path.join(themeDirectory, 'package.json');
+const pkg = JSON.parse(fs.readFileSync(pkgPath).toString());
 
-const wpConfigWatchIgnore = [];
-if (wpConfig.watchOptions?.ignored) {
-	const ignored = Array.isArray(wpConfig.watchOptions.ignored) ? wpConfig.watchOptions.ignored : [wpConfig.watchOptions.ignored];
-	wpConfigWatchIgnore.push(...ignored);
+class ThemePackageBuilderPlugin {
+
+	/**
+	 * @type {Record<string, any>}
+	 */
+	options = {};
+
+	/**
+	 * @type {Record<string, any>}
+	 */
+	defaults = {};
+
+	/**
+	 * @param {Record<string, any> | undefined} options
+	 */
+	constructor(options = {}) {
+		this.options = { ...this.defaults, ...options };
+	}
+	
+	/**
+	 * @param {webpack.Compiler} compiler
+	 */
+  apply(compiler) {
+    compiler.hooks.beforeRun.tap('ThemePackageBuilderPlugin', () => {
+			this.buildStyleHeader();
+			this.buildReadMeHeaders();
+			this.buildPot();
+		});
+		compiler.hooks.done.tap('ThemePackageBuilderPlugin', () => {
+			this.buildZip();
+		});
+  }
+
+	buildStyleHeader() {
+		const data = {
+			'Theme Name': pkg.themeName || pkg.name || '',
+			'Theme URI': pkg.homepage || '',
+			'Author': pkg.author.name || '',
+			'Author URI': pkg.author.url || '',
+			'Description': pkg.description || '',
+			'Version': pkg.version || '',
+			'License': pkg.license || '',
+			'License URI': 'LICENSE',
+			'Text Domain': pkg.name || '',
+			'Tags': Array.isArray(pkg.keywords) ? pkg.keywords.join(', ') : '',
+		};
+		let contents = '/*!\n';
+		for (const key in data) {
+			contents += `${key}: ${data[key]}\n`;
+		}
+		contents += '*/\n';
+		fs.writeFileSync('./style.css', contents);
+	}
+	
+	buildReadMeHeaders() {
+		const pkgName = pkg.themeName || pkg.name || '';
+		const contributorNames = pkg.author.name ? [pkg.author.name] : [];
+		if (Array.isArray(pkg.contributors)) {
+			pkg.contributors.forEach((contributor) => {
+				contributorNames.push(contributor.name);
+			});
+		}
+		const data = {
+			'Contributors': contributorNames.join(', '),
+			'Version': pkg.version,
+			'Requires at least': 'WordPress ' + pkg.wordpress.versionRequired,
+			'Tested up to': 'WordPress ' + pkg.wordpress.versionTested,
+			'License': pkg.license,
+			'License URI': 'LICENSE',
+			'Tags': Array.isArray(pkg.keywords) ? pkg.keywords.join(', ') : '',
+		};
+		// Build README.txt header.
+		let txtContent = `=== ${pkgName} ===\n\n`;
+		for (const key in data) {
+			txtContent += `${key}: ${data[key]}\n`;
+		}
+		txtContent += `\n${pkg.description}\n`;
+		fs.writeFileSync('./README.txt', txtContent);
+		// Build README.md header.
+		let mdContent = `# ${pkgName}\n\n`;
+		for (const key in data) {
+			mdContent += `**${key}:** ${data[key]}\n`;
+		}
+		mdContent += `\n${pkg.description}\n`;
+		fs.writeFileSync('./README.md', mdContent);
+	}
+
+	buildPot() {
+		execSync(`composer run translate . languages/${pkg.name}.pot`, { stdio: 'inherit' });
+	}
+
+	buildZip() {
+		// Don't use leading or slashes.
+		const ignores = [
+			'.github',
+			'.vscode',
+			'node_modules',
+			'vendor',
+			'scripts',
+			'src',
+			'.editorconfig',
+			'.eslintrc',
+			'.gitignore',
+			'.nvmrc',
+			'.prettierignore',
+			'.stylelintrc',
+			'composer.json',
+			'composer.lock',
+			'package-lock.json',
+			'package.json',
+			'phpcs.xml',
+			'README.md',
+			'webpack.config.js',
+			`${pkg.name}.zip`,
+		];
+		const zipper = new DirArchiver(themeDirectory, path.join(themeDirectory, `${pkg.name}.zip`), false, ignores);
+		zipper.createZip();
+	}
 }
 
-module.exports = {
+/**
+ * @type {webpack.Configuration}
+ */
+const config = {
 	...wpConfig,
 	watchOptions: {
 		...wpConfig.watchOptions,
 		ignored: [
-			...wpConfigWatchIgnore,
+			...(Array.isArray(wpConfig.watchOptions?.ignored) ? wpConfig.watchOptions.ignored : []),
 			// These globs must begin with '**/'.
 			// See: https://webpack.js.org/configuration/watch/#watchoptionsignored
 			'**/.github/**/*',
@@ -46,19 +169,12 @@ module.exports = {
 		],
 	},
 	plugins: [
-		...wpConfig.plugins,
+		...(Array.isArray(wpConfig.plugins) ? wpConfig.plugins : []),
 		new RemoveEmptyScriptsPlugin(),
-		{
-			apply: (compiler) => {
-				compiler.hooks.beforeCompile.tap('BeforeCompilePlugin', () => {
-					execSync('node scripts/package.mjs', { stdio: 'inherit' });
-					execSync('node scripts/translate.mjs', { stdio: 'inherit' });
-				});
-			},
-		},
+		new ThemePackageBuilderPlugin(),
 	],
 	entry: {
-		...wpConfig.entry,
+		...((typeof wpConfig.entry === 'object') ? wpConfig.entry : {}),
 		// Scripts.
 		'js/site.min': path.resolve(themeDirectory, 'src/js/site.ts'),
 		'js/admin.min': path.resolve(themeDirectory, 'src/js/admin.ts'),
@@ -72,5 +188,7 @@ module.exports = {
 		'css/editor.min': path.resolve(themeDirectory, 'src/scss/editor.scss'),
 		'css/customizer-preview.min': path.resolve(themeDirectory, 'src/scss/customizer-preview.scss'),
 		'css/customizer-controls.min': path.resolve(themeDirectory, 'src/scss/customizer-controls.scss'),
-	},
+	}
 };
+
+export default config;
