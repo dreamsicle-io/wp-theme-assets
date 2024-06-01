2b4d0dae91e808a56485c1e1fc66e5e5164f09a3
Noah Mason <noah@dreamsicle.io>
2024-06-01 12:45:51 -0400

    Adds boilerplate for customizer partial updates.

--- package/src/js/customizer-preview.ts
+++ package/src/js/customizer-preview.ts
@@ -1,3 +1,35 @@
 import { announceEnqueued } from './modules/utils';
 
-announceEnqueued('customizer-preview.js');
+(function() {
+
+	const { wp } = window;
+
+	announceEnqueued('customizer-preview.js');
+
+	function updateModules(container: HTMLElement) {
+		// Update all modules in the updated container here.
+	}
+
+	document.addEventListener('DOMContentLoaded', () => {
+
+		if (wp?.customize?.selectiveRefresh) {
+
+			// Update customizer partials.
+			wp.customize.selectiveRefresh.bind('partial-content-rendered', ({ container }) => {
+				if (container?.[0] instanceof HTMLElement) {
+					updateModules(container[0]);
+				}
+			});
+			
+			// Update customizer widgets.
+			wp.customize.selectiveRefresh.bind('sidebar-updated', partial => {
+				const container = document.getElementById(partial.sidebarId);
+				if (container?.[0] instanceof HTMLElement) {
+					updateModules(container[0]);
+				}
+			});
+
+		}
+	});
+
+})();
