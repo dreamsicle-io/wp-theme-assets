2f1f8b4ff1aa3b656f3ab66d72b06ada71716e63
Noah Mason <noah@dreamsicle.io>
2024-06-05 21:05:03 -0400

    Updates paths to files in github workflow env bash script.

--- package/.github/workflows/scripts/env.sh
+++ package/.github/workflows/scripts/env.sh
@@ -7,7 +7,7 @@
 # Get PHP version that satisfies the setup-php action version keys
 # by parsing the composer.json file's require.php config.
 
-RAW_PHP_VERSION=$(jq -r ".require.php" ../../composer.json)
+RAW_PHP_VERSION=$(jq -r ".require.php" composer.json)
 MAJOR_VERSION_PATTERN="^\^"
 PHP_VERSION=""
 if [[ $RAW_PHP_VERSION =~ $MAJOR_VERSION_PATTERN ]]; then
@@ -18,15 +18,15 @@ fi
 
 # Get the theme name from the package.json file.
 
-THEME_NAME=$(jq -r ".name" ../../package.json)
+THEME_NAME=$(jq -r ".name" package.json)
 
 # Get the theme version from the package.json file.
 
-THEME_VERSION=$(jq -r ".version" ../../package.json)
+THEME_VERSION=$(jq -r ".version" package.json)
 
 # Get the node version from the .nvmrc file.
 
-NODE_VERSION=$(cat ../../.nvmrc)
+NODE_VERSION=$(cat .nvmrc)
 
 # Set environment variables on $GITHUB_ENV.
 
